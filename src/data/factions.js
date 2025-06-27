1039f6c19e8b44a0f7e591b9cf81deb3c27c941a
WallaceMcGregor <wallace0mcgregor@gmail.com>
2025-06-27 08:02:29 +0200

    Crear componente FactionCard y estructura inicial de datos de facciones

---
+++ src/data/factions.js
@@ -0,0 +1,339 @@
+export const facciones = [
+{
+    nombre: "Los Hermanos de La Primera Llama",
+    descripcion: [
+      "Fanáticos de cualquier raza que tratan de erradicar toda oscuridad que mora en el Viejo Mundo.",
+      "Guiados por el primer hermano Ylsidor, un visionario elfo solar que lidera la purificación.",
+      "Instalados recientemente en la región.",
+      "Entregan a sus hermanos a las llamas. Si el elegido lo hace sin gritar, es signo de buen augurio."
+    ],
+    relojes: [
+      {
+        nombre: "Liberar esclavos los Garra Ponzoñosa",
+        imagen: "/images/reloj6.png"
+      },
+      {
+        nombre: "Reunir nuevos fieles para un futuro ritual de purificación",
+        imagen: "/images/reloj8.png"
+      }
+    ],
+    imagen: "/images/losHermanosDeLaPrimeraLlama.jpg",
+    bando: "luz",
+    nivel: 4
+  },
+  {
+    nombre: "Tribu Garra Ponzoñosa",
+    descripcion: [
+      "Goblins que creen que las ruinas contienen riquezas.",
+      "Poseen desnutridos esclavos enjaulados.",
+      "Utilizan a sus esclavos por las noches para rebuscar en las ruinas."
+    ],
+    relojes: [
+      {
+        nombre: "Encontrar un objeto valioso",
+        imagen: "/images/reloj8.png"
+      },
+      {
+        nombre: "Capturar más esclavos",
+        imagen: "/images/reloj6.png"
+      }
+    ],
+    imagen: "/images/tribuGarraPonzoñosa.jpg",
+    bando: "oscuridad",
+    nivel: 3
+  },
+  {
+    nombre: "Compañía del Tuerto",
+    descripcion: [
+      "Mercenarios veteranos y bien equipados.",
+      "Patrullan las rutas comerciales del Condado Marcaforte.",
+      "Les mueve el dinero y un contrato firmado (en ese orden)."
+    ],
+    relojes: [
+      {
+        nombre: "Patrullar los alrededores del Claro Sangrante",
+        imagen: "/images/reloj8.png"
+      },
+      {
+        nombre: "Capturar a un miembro del gremio de ladrones",
+        imagen: "/images/reloj4.png"
+      }
+    ],
+    imagen: "/images/companiaDelTuerto.jpg",
+    bando: "luz",
+    nivel: 3
+  },
+  {
+    nombre: "Espectros de Malumbra",
+    descripcion: [
+      "Los muertos sin descanso de una antigua batalla.",
+      "Almas en pena que no recuerdan qué hacen en este mundo y por qué siguen aquí.",
+      "Sus recuerdos se entremezclan con la realidad, viéndose desorientados y confusos."
+    ],
+    relojes: [
+      {
+        nombre: "Atraer a incautos a sus lugares de descanso",
+        imagen: "/images/reloj6.png"
+      },
+      {
+        nombre: "Incriminar a Esevok",
+        imagen: "/images/reloj4.png"
+      }
+    ],
+    imagen: "/images/espectrosDeMalumbra.jpg",
+    bando: "neutral",
+    nivel: 3
+  },
+  {
+    nombre: "Gnomos del aire",
+    descripcion: [
+      "Colonia de gnomos asentados en naves aerostáticas.",
+      "Son expertos mecánicos e inventores.",
+      "Van de un lado a otro ofreciendo sus servicios o vendiendo sus artefactos."
+    ],
+    relojes: [
+      {
+        nombre: "Obtener un acuerdo como transportistas para Elonkop",
+        imagen: "/images/reloj8.png"
+      },
+      {
+        nombre: "Desvelar la sede del gremio de ladrones",
+        imagen: "/images/reloj6.png"
+      }
+    ],
+    imagen: "/images/gnomosDelAire.jpg",
+    bando: "luz",
+    nivel: 3
+  },
+  {
+    nombre: "Guardabosques",
+    descripcion: [
+      "Un asentamiento de humanos exiliados que vigilaban el bosque.",
+      "Sus antiguos hogares fueron corrompidos por un origen desconocido, pero parece surgir de las profundidades, corrompiendo flora y fauna a su antojo.",
+      "Buscan recuperar los dominios perdidos por un mal que asola el bosque y lo ha ido corrompiendo."
+    ],
+    relojes: [
+      {
+        nombre: "Conseguir equipo de la Compañía del Tuerto (+nivel)",
+        imagen: "/images/reloj8.png"
+      },
+      {
+        nombre: "Rastrear la ubicación de Esevok",
+        imagen: "/images/reloj6.png"
+      }
+    ],
+    imagen: "/images/guardabosques.jpg",
+    bando: "luz",
+    nivel: 2
+  },
+  {
+    nombre: "Ar-tel-quessir",
+    descripcion: [
+      "Elfos solares que tienen sus hogares en un gran árbol.",
+      "Siembran semillas mediante la magia.",
+      "Tratan de hacer florecer estas semillas para repoblar su bosque marchito."
+    ],
+    relojes: [
+      {
+        nombre: "Obtener unas piedras rúnicas",
+        imagen: "/images/reloj6.png"
+      },
+      {
+        nombre: "Repoblar su bosque (+nivel)",
+        imagen: "/images/reloj8.png"
+      }
+    ],
+    imagen: "/images/arTelQuessir.jpg",
+    bando: "luz",
+    nivel: 2
+  },
+  {
+    nombre: "Gremio de ladrones",
+    descripcion: [
+      "Grupo de robacaras que la mayoría piensa que son vulgares ladrones.",
+      "Aunque casi nadie lo sabe, están asentados en la antigua torre, con planes de reconstruirla.",
+      "Quieren convertir la torre en un puesto fronterizo que controle el acceso a la región."
+    ],
+    relojes: [
+      {
+        nombre: "Chantajear a El Tuerto",
+        imagen: "/images/reloj8.png"
+      },
+      {
+        nombre: "Realizar atracos en Marcaforte (+nivel)",
+        imagen: "/images/reloj6.png"
+      }
+    ],
+    imagen: "/images/gremioDeLadrones.jpg",
+    bando: "neutral",
+    nivel: 2
+  },
+  {
+    nombre: "Esevok el Devorador",
+    descripcion: [
+      "Liche con un pequeño grupo de no muertos.",
+      "Está haciendo los preparativos para crear una horda de no muertos.",
+      "Fue un antiguo mago que falleció en una batalla legendaria al sur de la región."
+    ],
+    relojes: [
+      {
+        nombre: "Saquear el cementerio de Marcaforte.",
+        imagen: "/images/reloj6.png"
+      },
+      {
+        nombre: "Reanimar cadáveres (+nivel)",
+        imagen: "/images/reloj8.png"
+      }
+    ],
+    imagen: "/images/esevokElDevorador.jpg",
+    bando: "oscuridad",
+    nivel: 2
+  },
+  {
+    nombre: "Silentes",
+    descripcion: [
+      "Ratas extraordinariamente grandes e inteligentes.",
+      "Se comunican de una manera extraña que solo unos pocos comprende.",
+      "Vinieron por el vórtice y quieren volver a él, aunque es un proceso místico."
+    ],
+    relojes: [
+      {
+        nombre: "Adquirir cadáveres de Malumbra",
+        imagen: "/images/reloj6.png"
+      },
+      {
+        nombre: "Perturbar las visiones de los Hermanos de la Primera Llama",
+        imagen: "/images/reloj6.png"
+      }
+    ],
+    imagen: "/images/silentes.jpg",
+    bando: "neutral",
+    nivel: 1
+  },
+  {
+    nombre: "Teu-tel-quessir",
+    descripcion: [
+      "Elfos lunares que decoran sus hogares con huesos de animales para ahuyentar a los curiosos.",
+      "Mantienen un ritual con el que evitar la corrupción en sus dominios.",
+      "Anhelan que su antigua tierra vuelva a florecer como antaño."
+    ],
+    relojes: [
+      {
+        nombre: "Conseguir semillas de los elfos solares (+nivel).",
+        imagen: "/images/reloj6.png"
+      },
+      {
+        nombre: "Sacrificar Hermanos de la Primera Llama.",
+        imagen: "/images/reloj8.png"
+      }
+    ],
+    imagen: "/images/teuTelQuessir.jpg",
+    bando: "oscuridad",
+    nivel: 1
+  },
+  {
+    nombre: "Condado de Marcaforte",
+    descripcion: [
+      "Una región organizada y civilizada, que abarca tierras y asentamientos.",
+      "Busca la prosperidad a través del comercio y los acuerdos, y protege sus intereses.",
+      "Es una entidad más grande que las facciones individuales, influenciando la estabilidad regional."
+    ],
+    relojes: [
+      {
+        nombre: "Construir un puerto fluvial",
+        imagen: "/images/reloj8.png"
+      },
+      {
+        nombre: "Entablar un acuerdo con los gnomos del aire",
+        imagen: "/images/reloj6.png"
+      }
+    ],
+    imagen: "/images/condadoDeMarcaforte.jpg",
+    bando: "luz",
+    nivel: 4
+  },
+  {
+    nombre: "Academia de magos",
+    descripcion: [
+      "Un centro dedicado al estudio y la práctica de las artes arcanas.",
+      "Se esfuerzan por aprisionar entidades peligrosas y obtener conocimientos mágicos de otras razas.",
+      "Su enfoque está en la acumulación de saber y poder a través de la magia. (Esta descripción es una inferencia basada en sus relojes y el rol típico de una academia de magos en fantasía, ya que no hay una descripción explícita en las 'Facciones del Viejo Mundo')."
+    ],
+    relojes: [
+      {
+        nombre: "Aprisionar a un espectro de Malumbra",
+        imagen: "/images/reloj8.png"
+      },
+      {
+        nombre: "Obtener abalorios de los elfos lunares",
+        imagen: "/images/reloj6.png"
+      }
+    ],
+    imagen: "/images/academiaDeMagos.jpg",
+    bando: "neutral",
+    nivel: 3
+  },
+  {
+    nombre: "Granjeros de Marcaforte",
+    descripcion: [
+      "Comunidades agrícolas que proveen recursos al Condado Marcaforte.",
+      "Buscan mejorar sus condiciones y ejercer influencia en la región a través de la cohesión comunitaria.",
+      "Son gente sencilla y trabajadora, fundamental para el sustento de la región. (Esta descripción es una inferencia basada en sus relojes y el rol típico de granjeros en fantasía, ya que no hay una descripción explícita en las 'Facciones del Viejo Mundo', aunque se mencionan 'Granjeros' en otros contextos)."
+    ],
+    relojes: [
+      {
+        nombre: "Convencer a los habitantes de Marcaforte (+nivel)",
+        imagen: "/images/reloj6.png"
+      },
+      {
+        nombre: "Organizar colectas de dinero",
+        imagen: "/images/reloj4.png"
+      }
+    ],
+    imagen: "/images/granjerosDeMarcaforte.jpg",
+    bando: "luz",
+    nivel: 1
+  },
+  {
+    nombre: "Clan Claro Sangrante",
+    descripcion: [
+      "Un clan misterioso que realiza sacrificios para repeler a aquellos que considera una amenaza.",
+      "Muestra hostilidad hacia los guardabosques y busca someter a otros poderes, como la Corte del Amanecer.",
+      "Su existencia está ligada a rituales y prácticas que ahuyentan a sus enemigos. (Esta descripción es una inferencia basada en sus relojes, ya que no hay una descripción explícita en las 'Facciones del Viejo Mundo')."
+    ],
+    relojes: [
+      {
+        nombre: "Realizar un sacrificio que ahuyente a los guardabosques",
+        imagen: "/images/reloj6.png"
+      },
+      {
+        nombre: "Capturar a la reina de la Corte del Amanecer",
+        imagen: "/images/reloj8.png"
+      }
+    ],
+    imagen: "/images/clanClaroSangrante.jpg",
+    bando: "oscuridad",
+    nivel: 2
+  },
+  {
+    nombre: "Corte del Amanecer",
+    descripcion: [
+      "Una facción involucrada en rituales, posiblemente de naturaleza mística o espiritual.",
+      "Busca establecer una conexión simbiótica con las Silentes.",
+      "Sus propósitos se centran en ritos que pueden influir en el mundo, como el Rito del Amanecer. (Esta descripción es una inferencia basada en sus relojes, ya que no hay una descripción explícita en las 'Facciones del Viejo Mundo')."
+    ],
+    relojes: [
+      {
+        nombre: "Completar el Rito del Amanecer (+nivel)",
+        imagen: "/images/reloj8.png"
+      },
+      {
+        nombre: "Entrar en simbiosis con un Silente",
+        imagen: "/images/reloj6.png"
+      }
+    ],
+    imagen: "/images/corteDelAmanecer.jpg",
+    bando: "neutral",
+    nivel: 3
+  }
+];
