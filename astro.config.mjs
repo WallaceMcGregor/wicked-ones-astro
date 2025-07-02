227b26b0249502f745c340403cffbdc5681d8022
WallaceMcGregor <wallace0mcgregor@gmail.com>
2025-07-02 06:41:35 +0200

    Configurar Iconify para que se muestren todos los iconos

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  compilerOptions: {
+    tags: {
+      customElements: ['iconify-icon'],
+    }
+  }
 });
